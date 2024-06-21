df14225dde76199769da8691a785ae19c9a640c7
Ian Baumeister <ian@avotravel.com>
2024-06-21 15:49:48 +0200

    Added http, fs, url, path modules, createServer, read and serve files functions

--- movie_api/server.js
+++ movie_api/server.js
@@ -1,10 +1,38 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
 http
   .createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello Node!\n");
+    const parsedUrl = url.parse(request.url, true);
+    const pathname = parsedUrl.pathname;
+
+    if (pathname.includes("documentation")) {
+      fs.readFile(
+        path.join(__dirname, "documentation.html"),
+        "utf-8",
+        (err, data) => {
+          if (err) {
+            response.writeHead(500, { "Content-Type": "text/plain" });
+            response.end("500 - Internal Server Error");
+          } else {
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.end(data);
+          }
+        }
+      );
+    } else {
+      fs.readFile(path.join(__dirname, "index.html"), "utf-8", (err, data) => {
+        if (err) {
+          response.writeHead(500, { "Content-Type": "text/plain" });
+          response.end("500 - Internal Server Error");
+        } else {
+          response.writeHead(200, { "Content-Type": "text/html" });
+          response.end(data);
+        }
+      });
+    }
   })
   .listen(8080);
 
