ddaf65efa7938cf517f7aac089eee6db9b55d727
Ian Baumeister <ian@avotravel.com>
2024-06-27 00:17:34 +0200

    minor indentation edits

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.static("public"));
 // Define a route to serve the documentation.html file
 // Accessible via localhost:8080/documentation.html
 // This route is now handled by express.static middleware
+
 app.get("/", (req, res) => {
   res.send(
     "Welcome to my Top Movies List / API. The art of film is relatively new, but it had given both the creators and spectators countless possibilities while creating those masterpieces, as well as fantastic experiences for the ones enjoying watching them. My Top Movies List includes just the movies I could recall right away. I hope you'd get a taste to watch some of them and enjoy them as much as I did."
