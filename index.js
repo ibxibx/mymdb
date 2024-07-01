4a10c5122de75d26ede6c974c8df83fb0fa96090
Ian Baumeister <ian@avotravel.com>
2024-07-02 00:47:46 +0200

    Updated bodyParser middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const port = 8080;
+const bodyParser = require("body-parser");
 
 // Use Morgan Middleware to log requests
 app.use(morgan("dev"));
