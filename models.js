7bd153d848b8bfe40ae7cd273765c7e473a506d0
Ian Baumeister <ian@avotravel.com>
2024-07-30 21:28:58 +0200

    Updated the schemas with timestamps, minor edits

--- models.js
+++ models.js
@@ -1,25 +1,32 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const { Schema } = mongoose;
 
-let movieSchema = new Schema({
-  Title: { type: String, required: true },
-  Description: { type: String, required: true },
-  GenreId: { type: mongoose.Schema.Types.ObjectId, ref: "Genre" },
-  DirectorId: { type: mongoose.Schema.Types.ObjectId, ref: "Director" },
-  Actors: [String],
-  ImagePath: String,
-  Featured: Boolean,
-});
+let movieSchema = new Schema(
+  {
+    Title: { type: String, required: true },
+    Description: { type: String, required: true },
+    GenreId: { type: mongoose.Schema.Types.ObjectId, ref: "Genre" },
+    DirectorId: { type: mongoose.Schema.Types.ObjectId, ref: "Director" },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean,
+  },
+  { timestamps: true }
+);
 
-let userSchema = mongoose.Schema({
-  Username: { type: String, required: true },
-  Password: { type: String, required: true },
-  Email: { type: String, required: true, unique: true },
-  Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
-});
+let userSchema = new mongoose.Schema(
+  {
+    Username: { type: String, required: true },
+    Password: { type: String, required: true },
+    Email: { type: String, required: true, unique: true },
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
+  },
+  { timestamps: true }
+);
 
-userSchema.statics.hashPassword = (password) => {
+userSchema.statics.hashPassword = function (password) {
   return bcrypt.hashSync(password, 10);
 };
 
@@ -27,17 +34,23 @@ userSchema.methods.validatePassword = function (password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
-let genreSchema = new Schema({
-  Name: { type: String, required: true },
-  Description: String,
-});
+let genreSchema = new Schema(
+  {
+    Name: { type: String, required: true },
+    Description: String,
+  },
+  { timestamps: true }
+);
 
-let directorSchema = new Schema({
-  Name: { type: String, required: true },
-  Bio: String,
-  Birth: Date,
-  Death: { type: Date, default: null }, // Allowing null values for Death
-});
+let directorSchema = new Schema(
+  {
+    Name: { type: String, required: true },
+    Bio: String,
+    Birth: Date,
+    Death: { type: Date, default: null }, // Allowing null values for Death
+  },
+  { timestamps: true }
+);
 
 let Movie = mongoose.model("Movie", movieSchema);
 let User = mongoose.model("User", userSchema);
