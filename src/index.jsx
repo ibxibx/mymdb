49b6c611f61d83ee3d5a2bd47a08dda6b1fd26df
Ian Baumeister <ian@avotravel.com>
2024-08-06 17:38:07 +0200

    Renamed MyFlixApplication to App in render

--- src/index.jsx
+++ src/index.jsx
@@ -13,4 +13,4 @@ const container = document.querySelector("#root");
 const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
-root.render(<MyFlixApplication />);
+root.render(<App />);
