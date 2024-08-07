936f6fee4cb75839d78bf00579ef8cfa56ad81f7
Ian Baumeister <164527551+ibxibx@users.noreply.github.com>
2024-08-07 23:25:54 +0200

    Updated main-view.jsx with my movie API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,10 +1,32 @@
-import { useState } from "react";
-import { MovieCard } from "../movie-card/movie-card";
-import { MovieView } from "../movie-view/movie-view";
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+import MovieView from "./MovieView";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([]);  // Initialize with an empty array
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://mymdb-api.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((movie) => {
+          return {
+            id: movie._id,
+            title: movie.Title,
+            description: movie.Description,
+            genre: movie.Genre.Name,
+            director: movie.Director.Name,
+            image: movie.ImagePath,
+          };
+        });
+
+        setMovies(moviesFromApi);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
