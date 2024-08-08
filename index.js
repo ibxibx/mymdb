dc0fb1a617bc1671392202f2d8f5c07f83d97107
Ian Baumeister <ian@avotravel.com>
2024-08-08 22:44:26 +0200

    Allowed all origins for API - just to test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = = ['*'];
 let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
@@ -78,14 +78,17 @@ mongoose.connect(mongoUri, {
   });
 
 app.use(cors({
- origin: (origin, callback) => {
-   if(!origin) return callback(null, true);
-   if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-     let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-     return callback(new Error(message ), false);
-   }
-   return callback(null, true);
- }
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf('*') !== -1) {
+      return callback(null, true);
+    }
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 app.use(cors());
