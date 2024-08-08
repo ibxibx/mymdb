6393f4552dfae0ae1e3f4e2a68cb393d20c7d716
Ian Baumeister <164527551+ibxibx@users.noreply.github.com>
2024-08-08 02:04:12 +0200

    Updated index.js for cors in detail and allowedorigins

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 require("./passport");
 
 const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 const morgan = require("morgan");
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -76,7 +77,17 @@ mongoose.connect(mongoUri, {
     console.error("Error connecting to the database", error);
   });
 
-app.use(cors());
+app.use(cors({
+ origin: (origin, callback) => {
+   if(!origin) return callback(null, true);
+   if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+     let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+     return callback(new Error(message ), false);
+   }
+   return callback(null, true);
+ }
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
