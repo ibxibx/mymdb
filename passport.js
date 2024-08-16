482844fc42813bdf4c7a173d9a09f03a484846e4
Ian Baumeister <ian@avotravel.com>
2024-08-17 01:19:34 +0200

    Updated JWT and JWT in passport

--- passport.js
+++ passport.js
@@ -1,11 +1,11 @@
-const passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy,
-  Models = require('./models.js'),
-  passportJWT = require('passport-jwt');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const passportJWT = require('passport-jwt');
+const Models = require('./models.js');
 
 let Users = Models.User,
-  JWTStrategy = passportJWT.Strategy,
-  ExtractJWT = passportJWT.ExtractJwt;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 require('dotenv').config();
 
@@ -38,15 +38,29 @@ passport.use(
   )
 );
 
-passport.use(new JWTStrategy({
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWT_SECRET // Use environment variable here
-}, async (jwtPayload, callback) => {
-  return await Users.findById(jwtPayload._id)
-    .then((user) => {
-      return callback(null, user);
-    })
-    .catch((error) => {
-      return callback(error);
-    });
-}));
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET, // Use environment variable here
+    },
+    async (jwtPayload, callback) => {
+      try {
+        // Find user by ID in JWT payload
+        const user = await Users.findById(jwtPayload._id);
+        if (!user) {
+          console.log('User not found');
+          return callback(null, false, { message: 'User not found.' });
+        }
+
+        console.log('JWT validated, user found');
+        return callback(null, user);
+      } catch (error) {
+        console.log('Error in JWTStrategy:', error);
+        return callback(error);
+      }
+    }
+  )
+);
+
+module.exports = passport;
