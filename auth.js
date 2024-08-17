9a42d1a595aeff6d6eef16a9a567d4c7315650ba
Ian Baumeister <ian@avotravel.com>
2024-08-17 21:31:27 +0200

    Updated auth.js to fix the login issue

--- auth.js
+++ auth.js
@@ -1,23 +1,27 @@
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-require('./passport');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+require("./passport");
+require("dotenv").config();
 
 let generateJWTToken = (user) => {
-  return jwt.sign({ id: user._id, username: user.Username }, process.env.JWT_SECRET, {
-    subject: user.Username,
-    expiresIn: '365d',
-    algorithm: 'HS256'
-  });
-}
+  return jwt.sign(
+    { id: user._id, username: user.Username },
+    process.env.JWT_SECRET,
+    {
+      subject: user.Username,
+      expiresIn: "365d",
+      algorithm: "HS256",
+    }
+  );
+};
 
 module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
+  router.post("/login", (req, res) => {
+    passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: 'Something is not right',
-          user: user
+          message: "Something is not right",
+          user: user,
         });
       }
       req.login(user, { session: false }, (error) => {
@@ -29,4 +33,4 @@ module.exports = (router) => {
       });
     })(req, res);
   });
-}
+};
