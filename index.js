9b8bde7065e9c34b6c0f8097dc907d782ed6bf96
Ian Baumeister <ian@avotravel.com>
2024-08-28 22:58:54 +0200

    Removed the redundant route app.put users/:username

--- index.js
+++ index.js
@@ -624,35 +624,6 @@ app.put(
   }
 );
 
-app.put(
-  "/users/:Username",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    if (req.user.Username !== req.params.Username) {
-      return res.status(400).send("Permission denied");
-    }
-    await Users.findOneAndUpdate(
-      { Username: req.params.Username },
-      {
-        $set: {
-          Username: req.body.Username,
-          Password: req.body.Password,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday,
-        },
-      },
-      { new: true }
-    )
-      .then((updatedUser) => {
-        res.json(updatedUser);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
-
 /**
  * @swagger
  * /users/{userId}/movies/{movieId}:
