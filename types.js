cf23a257e652516233a50c8e12f1af0458a524cb
Ian Baumeister <ian@avotravel.com>
2024-11-04 14:55:06 +0100

    Added jsdoc documentation

---
+++ types.js
@@ -0,0 +1,14 @@
+/**
+ * @typedef {Object} Request
+ * @description Express request object
+ */
+
+/**
+ * @typedef {Object} Response
+ * @description Express response object
+ */
+
+/**
+ * @typedef {Function} NextFunction
+ * @description Express next middleware function
+ */
